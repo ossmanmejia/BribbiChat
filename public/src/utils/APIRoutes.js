43c85e92380df91abe6f593b042b754bb2180bcb
ossmanmejia <79607140+ossmanmejia@users.noreply.github.com>
2023-01-10 14:29:23 -0500

    Deploy_2023

--- public/src/utils/APIRoutes.js
+++ public/src/utils/APIRoutes.js
@@ -1,4 +1,4 @@
-export const host = "https://bribii-chat.herokuapp.com";
+export const host = "https://bribbichat-production.up.railway.app/";
 export const loginRoute = `${host}/api/auth/login`;
 export const registerRoute = `${host}/api/auth/register`;
 export const logoutRoute = `${host}/api/auth/logout`;
